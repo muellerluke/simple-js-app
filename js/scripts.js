6c6b3b3750426c64ce62c42a14451a78cf426dc9
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-06-07 18:31:52 -0500

    Add styling and output Pokedex with a loop and conditional statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 var pokemonList = [
-  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}
-  {name: 'Golem', height: 1.4, types: ['rock', 'ground']}
+  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+  {name: 'Golem', height: 1.4, types: ['rock', 'ground']},
   {name: 'Wailord', height: 14.5, types: ['water']}
 ];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  if (pokemonList[i].height > 10) document.write(" - Wow, that's big! ")
+  document.write("<br><br>");
+}
