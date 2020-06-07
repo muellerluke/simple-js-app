35532724f924299271265d71e018b6f40d3ee01f
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-06-07 16:09:09 -0500

    Added Pokemon
    Added three pokemon to the array of objects with data for each.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-var favoriteFood = 'hamburger';
-document.write(favoriteFood);
+var pokemonList = [
+  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']}
+  {name: 'Golem', height: 1.4, types: ['rock', 'ground']}
+  {name: 'Wailord', height: 14.5, types: ['water']}
+];
