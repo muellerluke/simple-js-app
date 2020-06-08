163d62e586930f284599612cfc9e9cda0190c4b9
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-06-08 12:08:53 -0500

    implemented IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-var pokemonList = [
-  {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-  {name: 'Golem', height: 1.4, types: ['rock', 'ground']},
-  {name: 'Wailord', height: 14.5, types: ['water']}
-];
+var pokemonRepository = (function(){
+  var pokemonList = [
+    {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+    {name: 'Golem', height: 1.4, types: ['rock', 'ground']},
+    {name: 'Wailord', height: 14.5, types: ['water']}
+  ];
+
+  function add(pokemon) {
+    if(typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'types' in pokemon) pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})()
 
 function pokedex_output(pokemon){
   document.write(pokemon.name + " (height: " + pokemon.height + ")");
@@ -10,4 +25,4 @@ function pokedex_output(pokemon){
   document.write("<br><br>");
 }
 
-pokemonList.forEach(pokedex_output);
+pokemonRepository.getAll().forEach(pokedex_output);
