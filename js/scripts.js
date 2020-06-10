7444cb7f65eda9876fd4f58474375bdf956d4be3
muellerluke <66047826+muellerluke@users.noreply.github.com>
2020-06-09 19:20:58 -0500

    implemented modal for showing data

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,20 @@ var pokemonRepository = (function(){
     button.classList.add('list-item-button');
     listItem.appendChild(button);
     list.appendChild(listItem);
+    //When you click on a pokemon...
     button.addEventListener('click', () => showDetails(pokemon));
   }
-
+  //add pokemon to the pokemonList
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
 
+  //get all pokemon from the list
   function getAll() {
     return pokemonList;
   }
 
+  //load pokemon from the api
   function loadList() {
       return fetch(apiUrl).then(function (response) {
         return response.json();
@@ -36,7 +39,7 @@ var pokemonRepository = (function(){
         console.error(e);
       })
     }
-
+    // when clicked on, load details from the api about the pokemon
     function loadDetails(item) {
     var url = item.detailsUrl;
     return fetch(url).then(function (response) {
@@ -51,10 +54,60 @@ var pokemonRepository = (function(){
     });
   }
 
+  //show the loaded details about the pokemon
   function showDetails(item) {
-  loadDetails(item).then(function () {
-    console.log(item);
+    loadDetails(item).then(function () {
+      showModal(item);
+    });
+  }
+
+  function showModal(item) {
+    var modalContainer = document.querySelector('#modal-container');
+
+    modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    var titleElement = document.createElement('h1');
+    titleElement.innerText = item.name;
+
+    var imgElement = document.createElement('img');
+    imgElement.setAttribute('src', item.imageUrl);
+
+    var contentElement = document.createElement('p');
+    contentElement.innerText = item.name + '\'s height: ' + item.height;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imgElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+    }
   });
+
+  modalContainer.addEventListener('click', (e) => {
+    var target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+      }
+    });
+  }
+
+  function hideModal(){
+    var modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
   }
 
   return {
